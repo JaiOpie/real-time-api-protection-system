5df5b00be5f9f5e05a2ffa2c8366cd55d6ae00bd
Jatin <you@example.com>
2026-03-05 00:10:02 +0530

    Implemented Auto-unblocking APIkey

--- api-gateway/src/main/java/com/project/api_gateway/service/BlockedKeyService.java
+++ api-gateway/src/main/java/com/project/api_gateway/service/BlockedKeyService.java
@@ -1,21 +1,42 @@
 package com.project.api_gateway.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class BlockedKeyService {
 
-    private final Set<String> blockedKeys = ConcurrentHashMap.newKeySet();
+    private final long cooldownMillis;
+
+    private final Map<String, Long> blockedKeys = new ConcurrentHashMap<>();
+
+    public BlockedKeyService(
+            @Value("${api.block.cooldown-millis}") long cooldownMillis) {
+        this.cooldownMillis = cooldownMillis;
+    }
 
     public void block(String apiKey){
-        blockedKeys.add(apiKey);
+        blockedKeys.put(apiKey,System.currentTimeMillis());
     }
 
     public boolean isBlocked(String apiKey){
-        return blockedKeys.contains(apiKey);
+        if(!blockedKeys.containsKey(apiKey)) return false;
+
+        long blockedAt = blockedKeys.get(apiKey);
+        long now = System.currentTimeMillis();
+
+        if(now - blockedAt > cooldownMillis){
+            blockedKeys.remove(apiKey);
+            System.out.println("Auto-unblocked API key: " + apiKey);
+            return false;
+        }
+        return true;
     }
+
+
 
 }

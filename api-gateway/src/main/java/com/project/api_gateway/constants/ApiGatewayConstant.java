dde9749424ea589f76115876b267f10fbe0d7829
Jatin <you@example.com>
2026-03-06 01:46:34 +0530

    Implemented Redis based Auto Unblocking

--- api-gateway/src/main/java/com/project/api_gateway/constants/ApiGatewayConstant.java
+++ api-gateway/src/main/java/com/project/api_gateway/constants/ApiGatewayConstant.java
@@ -3,4 +3,5 @@ package com.project.api_gateway.constants;
 public class ApiGatewayConstant {
     public static final String API_KEY_HEADER = "X-API-KEY";
     public static final String VALID_API_KEY = "12345-SECRET-KEY";
+    public static final String PREFIX = "blocked:";
 }
--- api-gateway/src/main/java/com/project/api_gateway/service/BlockedKeyService.java
+++ api-gateway/src/main/java/com/project/api_gateway/service/BlockedKeyService.java
@@ -1,40 +1,39 @@
 package com.project.api_gateway.service;
 
+import com.project.api_gateway.constants.ApiGatewayConstant;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.Time;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class BlockedKeyService {
 
-    private final long cooldownMillis;
+    @Value("${api.block.cooldown-millis}")
+    private long cooldownMillis;
 
-    private final Map<String, Long> blockedKeys = new ConcurrentHashMap<>();
-
-    public BlockedKeyService(
-            @Value("${api.block.cooldown-millis}") long cooldownMillis) {
-        this.cooldownMillis = cooldownMillis;
-    }
+    private final StringRedisTemplate stringRedisTemplate;
 
     public void block(String apiKey){
-        blockedKeys.put(apiKey,System.currentTimeMillis());
+        stringRedisTemplate.opsForValue().set(
+                ApiGatewayConstant.PREFIX+apiKey,
+                "blocked",
+                cooldownMillis,
+                TimeUnit.MILLISECONDS
+        );
+        System.out.println("API key blocked in Redis: " + apiKey);
     }
 
     public boolean isBlocked(String apiKey){
-        if(!blockedKeys.containsKey(apiKey)) return false;
-
-        long blockedAt = blockedKeys.get(apiKey);
-        long now = System.currentTimeMillis();
-
-        if(now - blockedAt > cooldownMillis){
-            blockedKeys.remove(apiKey);
-            System.out.println("Auto-unblocked API key: " + apiKey);
-            return false;
-        }
-        return true;
+      Boolean exists = stringRedisTemplate.hasKey(ApiGatewayConstant.PREFIX + apiKey);
+      return exists!=null && exists;
     }
 
 
